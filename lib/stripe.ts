f0db5fdae57c287758cb63596dc45af41e4192d0
Ayesha0300 <ayeshabashir030@gmail.com>
2025-01-29 22:12:41 +0500

    commit 3 #

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,8 +1,12 @@
 import Stripe from 'stripe';
 import { loadStripe } from '@stripe/stripe-js';
 
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('STRIPE_SECRET_KEY is not set');
+}
+
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2025-01-27.acacia', // Update to a valid API version
+  apiVersion: '2025-01-27.acacia',
 });
 
 export const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!); 
